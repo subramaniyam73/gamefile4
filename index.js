74b6773e79256bdafc8469598bc304e0647dea88
subramaniyam73 <63327623+subramaniyam73@users.noreply.github.com>
2020-04-23 05:49:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -326,8 +326,6 @@ for(let j=1;j<=20;j++)
     }
     else{
       document.querySelector("#b"+j).classList.add("cover");
-      audioObject2.pause();
-      audioObject3.pause();
     }
 
     count++;
